dc71f176af8cbfe19ffbd7ef41bea9ebd649ff83
skravchuk <ext-serhii.krachuk@here.com>
2025-01-25 23:21:05 +0200

    47 - adjusted Default according to the updated logic; adjusted tests; expanded Java Docs

--- src/main/java/com/codeus/winter/config/AutowireCapableBeanFactory.java
+++ src/main/java/com/codeus/winter/config/AutowireCapableBeanFactory.java
@@ -7,8 +7,10 @@ public abstract class AutowireCapableBeanFactory implements BeanFactory {
 
     /**
      * Resolves Bean instance using given {@link DependencyDescriptor}.
+     * Can collect multiple candidates beans into a List, Set or Map.
+     * Formed Map contains bean name as a key, bean instance as a value.
      *
-     * @param descriptor a dependency descriptor for resolving a bean.
+     * @param descriptor a dependency descriptor to resolve a bean.
      * @return bean instance that conform the given {@link DependencyDescriptor}.
      */
     protected abstract Object resolveDependency(DependencyDescriptor descriptor);
